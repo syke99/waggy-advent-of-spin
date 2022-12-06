889844665a976dea99620f2c4fbc88e3fe6c4658
Quinn Millican <quinn.millican@tpginc.com>
2022-12-06 08:16:45 -0600

    finished challenge 1, prep for challenge 2

---
+++ challenge-1/go.mod
@@ -0,0 +1,7 @@
+module github.com/waggy_advent_of_spin/challenge_1
+
+go 1.17
+
+require github.com/fermyon/spin/sdk/go v0.6.0
+
+require github.com/syke99/waggy v0.4.3
---
+++ challenge-1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/syke99/waggy"
+	"net/http"
+
+	spinhttp "github.com/fermyon/spin/sdk/go/http"
+)
+
+var flg waggy.FullCGI
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		helloWorldJSONBytes := []byte("{ \"message\":\"Hello, world\" }")
+
+		helloHandler := func(w http.ResponseWriter, r *http.Request) {
+			waggy.WriteDefaultResponse(w, r)
+		}
+
+		handler := waggy.InitHandlerWithRoute("/hello", &flg).
+			WithDefaultResponse(helloWorldJSONBytes).
+			MethodHandler(http.MethodGet, helloHandler)
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func main() {}
