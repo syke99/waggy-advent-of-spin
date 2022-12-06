e0b72d50cc182f5ef5b8a8f3b284b7ea7749ffb1
Quinn Millican <quinn.millican@tpginc.com>
2022-12-06 12:47:11 -0600

    more progress on Challenge 2

--- challenge-2/challenge-2-hello/main.go
+++ challenge-2/challenge-2-hello/main.go
@@ -3,48 +3,64 @@ package main
 import (
 	"bytes"
 	"fmt"
+	spinhttp "github.com/fermyon/spin/sdk/go/http"
 	"github.com/syke99/waggy"
 	"io/ioutil"
 	"net/http"
 	"strings"
-
-	spinhttp "github.com/fermyon/spin/sdk/go/http"
 )
 
 var flg waggy.FullCGI
 
 func init() {
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
-		name := waggy.Vars(r)["name"]
-
-		helloElfJSONBytes := []byte("{ \"message\":\"Hello, world\" }")
-
-		if name != "" {
-			lowerCaseName := ""
-
-			resp, err := spinhttp.Post("127.0.0.1:3001/lowercase", "application/json", bytes.NewBufferString(fmt.Sprintf("{ \"message\": %s", name)))
-			if err != nil {
-				waggy.WriteDefaultErrorResponse(w, r)
-			}
-			defer resp.Body.Close()
-
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				waggy.WriteDefaultErrorResponse(w, r)
-			}
-
-			lowerCaseName = strings.Split(string(body), " ")[2:3][0]
-
-			if lowerCaseName[:1] == "\"" &&
-				lowerCaseName[len(lowerCaseName)-1:] == "\"" {
-				lowerCaseName = lowerCaseName[1 : len(lowerCaseName)-1]
-			}
-
-			helloElfJSONBytes = []byte(fmt.Sprintf("{ \"message\":\"Hello, %s\" }", lowerCaseName))
-		}
-
 		helloHandler := func(w http.ResponseWriter, r *http.Request) {
-			waggy.WriteDefaultResponse(w, r)
+			if r.URL.Path == "/" {
+				w.WriteHeader(http.StatusNotFound)
+				w.Header().Set("Content-Type", "text/plain")
+				fmt.Fprintln(w, "resource not found")
+				return
+			}
+
+			name := waggy.Vars(r)["name"]
+
+			helloElfJSON := "{ \"message\":\"Hello, world\" }"
+
+			if name != "" {
+				lowerCaseName := ""
+
+				resp, err := spinhttp.Post("127.0.0.1:3001/lowercase", "application/json", bytes.NewBufferString(fmt.Sprintf("{ \"message\": %s", name)))
+				if err != nil {
+					waggy.WriteDefaultErrorResponse(w, r)
+				}
+				defer resp.Body.Close()
+
+				body := make([]byte, 0)
+
+				if resp != nil {
+					body, err = ioutil.ReadAll(resp.Body)
+				}
+
+				if err != nil {
+					waggy.WriteDefaultErrorResponse(w, r)
+				}
+
+				splitBody := strings.Split(string(body), " ")
+
+				if len(splitBody) > 3 {
+					lowerCaseName = splitBody[2:3][0]
+				}
+
+				if len(lowerCaseName) != 0 &&
+					lowerCaseName[:1] == "\"" &&
+					lowerCaseName[len(lowerCaseName)-1:] == "\"" {
+					lowerCaseName = lowerCaseName[1 : len(lowerCaseName)-1]
+				}
+
+				helloElfJSON = fmt.Sprintf("{ \"message\":\"Hello, %s\" }", lowerCaseName)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprintln(w, helloElfJSON)
 		}
 
 		we := waggy.WaggyError{
@@ -54,8 +70,7 @@ func init() {
 			Instance: "/",
 		}
 
-		handler := waggy.InitHandlerWithRoute("/hello", &flg).
-			WithDefaultResponse(helloElfJSONBytes).
+		handler := waggy.InitHandlerWithRoute("/hello/{name}", &flg).
 			WithDefaultErrorResponse(we, http.StatusInternalServerError).
 			MethodHandler(http.MethodGet, helloHandler)
 
--- challenge-2/challenge-2-lowercase/go.mod
+++ challenge-2/challenge-2-lowercase/go.mod
@@ -4,5 +4,5 @@ go 1.17
 
 require (
 	github.com/fermyon/spin/sdk/go v0.6.0
-	github.com/syke99/waggy v0.4.5
+	github.com/syke99/waggy v0.4.6
 )
